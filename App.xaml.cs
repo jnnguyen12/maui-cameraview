c55641020ddf8a5b28d75f20be4b8cb04139876e
Joanne Nguyen <jnguyen@mobiledemand.com>
2024-10-10 10:02:03 -0500

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CameraViewExternalCameras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CameraViewExternalCameras
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
